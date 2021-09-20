74e80a97e6e626c4e5a118067dbfced475daece5
Kamil Pluciński <kamil.plucinski@deepsense.ai>
2021-09-20 15:52:24 +0200

    Add splitting cells

--- tutorials/sst/format_converter.py
+++ tutorials/sst/format_converter.py
@@ -1,7 +1,67 @@
-from typing import Dict
+from enum import Enum
+from typing import Dict, List
+
+from nbformat import NotebookNode, v4
+from nbformat.v4 import new_notebook, new_markdown_cell, new_code_cell
+
+
+def code_preprocessor(input_source: str) -> str:
+    return input_source.strip('\n')
+
+
+def markdown_preprocessor(input_source: str) -> str:
+    return input_source
+
+
+class CellProcessingStatus(Enum):
+    CODE = 1
+    MARKDOWN = 2
 
 
 class FormatConverter:
-    def py2ipynb(self, py_file_text: str) -> Dict:
-        json_output = {}
-        return json_output
+    type2func = {
+        CellProcessingStatus.CODE: new_code_cell,
+        CellProcessingStatus.MARKDOWN: new_markdown_cell,
+    }
+    type2preprocessor = {
+        CellProcessingStatus.CODE: code_preprocessor,
+        CellProcessingStatus.MARKDOWN: markdown_preprocessor,
+    }
+
+    def py2ipynb(self, py_file_text: str) -> NotebookNode:
+        cells = self.extract_cells(py_file_text)
+        notebook = new_notebook(cells=cells)
+        return notebook
+
+    def extract_cells(self, python_text_file) -> List[NotebookNode]:
+        cells = []
+
+        current_cell_type = CellProcessingStatus.CODE
+        cell_lines = []
+
+        for line in python_text_file.splitlines():
+            if not line.startswith('"""'):
+                cell_lines.append(line)
+                continue
+
+            if cell_lines:
+                new_cell = self.get_cell(cell_lines, current_cell_type)
+                cells.append(new_cell)
+                cell_lines = []
+
+            if current_cell_type == CellProcessingStatus.CODE:
+                current_cell_type = CellProcessingStatus.MARKDOWN
+            elif current_cell_type == CellProcessingStatus.MARKDOWN:
+                current_cell_type = CellProcessingStatus.CODE
+
+        if cell_lines:
+            new_cell = self.get_cell(cell_lines, current_cell_type)
+            cells.append(new_cell)
+
+        return cells
+
+    def get_cell(self, cell_lines: List[str], current_cell_type: CellProcessingStatus) -> NotebookNode:
+        source = '\n'.join(cell_lines)
+        processed_source = self.type2preprocessor[current_cell_type](source)
+        cell = self.type2func[current_cell_type](processed_source)
+        return cell
--- tutorials/sst/tests/test_py2ipynb.py
+++ tutorials/sst/tests/test_py2ipynb.py
@@ -1,23 +1,30 @@
 import json
 from pathlib import Path
+from pprint import pprint
 
+import pytest
 from deepdiff import DeepDiff
+from nbformat import NotebookNode
 
 from format_converter import FormatConverter
 
 STATIC_FILES = Path('tests/static')
 
 
-def test_trivial_mapping_md_code_md():
-    # TODO parametrized test
-    file_name = 'trivial_mapping_md_code_md'
+@pytest.mark.parametrize(
+    "file_name",
+    ['trivial_mapping_md_code_md', 'just_py_method'],
+)
+def test_trivial_mapping_md_code_md(file_name):
     python_file_path, json_file_path = Path(file_name + '.py'), Path(file_name + '.json')
 
     file_py = (STATIC_FILES / python_file_path).read_text()
+    transformed_py = FormatConverter().py2ipynb(file_py)
+
     file_json = (STATIC_FILES / json_file_path).read_text()
     json_dict = json.loads(file_json)
 
-    transformed_py = FormatConverter().py2ipynb(file_py)
-    diff = DeepDiff(json_dict, transformed_py)
+    diff = DeepDiff(json_dict['cells'], transformed_py['cells'], ignore_type_in_groups=[(NotebookNode, dict)],
+                    exclude_regex_paths=r"root\[\d+\]\['id'\]")
 
     assert diff == {}
