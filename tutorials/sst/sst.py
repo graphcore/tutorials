a571f71ffa03076985a98ee29507f3605ba75fb4
Kamil Pluciński <kamil.plucinski@deepsense.ai>
2021-09-21 15:40:08 +0200

    Add markdown output and enable running jupyter

--- tutorials/sst/sst.py
+++ tutorials/sst/sst.py
@@ -1,31 +1,34 @@
 from pathlib import Path
 
 import click
-from nbformat import v4
+from nbconvert import NotebookExporter, MarkdownExporter
 
-from format_converter import FormatConverter
-
-
-@click.group()
-def cli():
-    pass
+from format_converter import py2ipynb
 
 
-@cli.command()
-@click.option('--filename', '-f', required=True, type=Path,
+@click.command()
+@click.option('--source', '-s', required=True, type=Path,
               help='Absolute or relative path to python file to be converted')
 @click.option('--output', '-o', required=True, type=Path,
-              help='Absolute or relative path to output jupyter notebook file ')
-def py2ipynb(filename: Path, output: Path) -> None:
+              help='Absolute or relative path to output file without extension')
+@click.option('--type', '-f', type=click.Choice(['jupyter', 'markdown']), help='Desired output file type')
+@click.option('--execute/--no-execute', default=True, help='Flag whether the notebook is to be executed or not')
+def cli(source: Path, output: Path, type: str, execute: bool) -> None:
     """
     Description
     """
-    py_text = filename.read_text()
-    notebook = FormatConverter().py2ipynb(py_text)
+    py_text = source.read_text()
+    notebook = py2ipynb(py_text)
 
-    jsonform = v4.writes(notebook)
-    with open(output, "w") as fpout:
-        fpout.write(jsonform)
+    exporter = NotebookExporter() if type == 'jupyter' else MarkdownExporter()
+    if execute:
+        exporter.register_preprocessor('nbconvert.preprocessors.ExecutePreprocessor', enabled=True)
+
+    output_content, _ = exporter.from_notebook_node(notebook)
+    filename = str(output) + exporter.file_extension
+
+    with open(filename, "w") as fpout:
+        fpout.write(output_content)
 
 
 if __name__ == '__main__':
--- tutorials/sst/tests/test_sst.py
+++ tutorials/sst/tests/test_sst.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from pprint import pprint
 
 import pytest
 from deepdiff import DeepDiff
