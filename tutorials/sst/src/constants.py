44931940ee611179b85fc25b2253ffe10809dcfd
Kamil Pluciński <kamil.plucinski@deepsense.ai>
2021-09-22 12:23:42 +0200

    Refactor

--- tutorials/sst/src/constants.py
+++ tutorials/sst/src/constants.py
@@ -1,2 +1,11 @@
+from nbconvert import NotebookExporter, MarkdownExporter
+
+from src.python_exporter import PythonExporter
+
 EXECUTE_PREPROCESSOR = 'nbconvert.preprocessors.ExecutePreprocessor'
 CELL_SEPARATOR = '"""'
+TYPE2EXPORTER = {
+    'jupyter': NotebookExporter,
+    'markdown': MarkdownExporter,
+    'purepython': PythonExporter
+}
--- tutorials/sst/sst.py
+++ tutorials/sst/sst.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 
 import click
-from nbconvert import NotebookExporter, MarkdownExporter
+from nbconvert import NotebookExporter
 
-from src.constants import EXECUTE_PREPROCESSOR
+from src.constants import EXECUTE_PREPROCESSOR, TYPE2EXPORTER
 from src.format_converter import py_to_ipynb
-from src.python_exporter import PythonExporter
+
+
+def construct_output_filename(outputname: Path, extension: str, input_name: Path) -> Path:
+    filename = str(outputname) + extension
+    assert not filename == str(input_name), f'Your source file and the expected output file name are the same: ' \
+                                            f'{input_name}, specify different outfile name using --output flag.'
+    return Path(filename)
 
 
 @click.command()
@@ -22,21 +28,14 @@ def cli(source: Path, output: Path, type: str, execute: bool) -> None:
     py_text = source.read_text()
     notebook = py_to_ipynb(py_text)
 
-    typ2exporters = {
-        'jupyter': NotebookExporter,
-        'markdown': MarkdownExporter,
-        'purepython': PythonExporter
-    }
-
-    exporter = typ2exporters[type]()
+    exporter_class = TYPE2EXPORTER.get(type, NotebookExporter)
+    exporter = exporter_class()
     if execute:
         exporter.register_preprocessor(EXECUTE_PREPROCESSOR, enabled=True)
 
     output_content, _ = exporter.from_notebook_node(notebook)
-    filename = str(output) + exporter.file_extension
 
-    assert not filename == str(source), f'Your source file and the expected output file name are the same: {source}, ' \
-                                        f'specify different outfile name using --output flag.'
+    filename = construct_output_filename(outputname=output, extension=exporter.file_extension, input_name=source)
 
     with open(filename, "w") as fpout:
         fpout.write(output_content)
